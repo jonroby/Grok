da6c6a9d44a44e699b7dfb750035f5d38fda0260
jonroby <joncaroby@gmail.com>
2018-01-21 19:14:59 -0800

    pass in arg as array

--- posts/curry.js
+++ posts/curry.js
@@ -1,24 +1,41 @@
-function test(a, b, c) {
+function multiplySubtract(a, b, c) {
     return a * b - c;
 }
 
+multiplySubtract()
+
 function curry(fn) {
     let len = fn.length;
-    let args = [];
 
-    function step(length, arg) {
+    function step(length, args) {
         if (length === 0) {
             return fn(...args);
         } else {
             return function(x) {
                 args.push(x);
-                return step(length - 1);
+                return step(length - 1, args);
             };
         }
     }
 
-    return step(len);
+    return step(len, []);
 }
 
-let curriedTest = curry(test);
-let ans = curriedTest(10)(3)(2);
+function uncurry(fn) {
+    let final = null;
+    function step(f, args) {
+        if (typeof f() !== "function") {
+            final = f;
+        } else {
+            step(f)
+        }
+    }
+
+    return step()
+
+    return function(x, y, z)
+}
+
+let curriedMultiplySubtract = curry(multiplySubtract);
+let ans = curriedMultiplySubtract(10)(3)(2);
+console.log(ans);
