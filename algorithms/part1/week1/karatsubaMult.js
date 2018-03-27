892871275eb822cdcf570e601c00c94a5c97010d
jonroby <joncaroby@gmail.com>
2018-03-26 23:50:40 -0700

    try 2

--- algorithms/part1/week1/karatsubaMult.js
+++ algorithms/part1/week1/karatsubaMult.js
@@ -1,33 +1,21 @@
-let num1 = '3141592653589793238462643383279502884197169399375105820974944592'
-let num2 = '2718281828459045235360287471352662497757247093699959574966967627'
+let num1 = 3141592653589793238462643383279502884197169399375105820974944592;
+let num2 = 2718281828459045235360287471352662497757247093699959574966967627;
 
-x = []
-y = []
-
-for (let i = 0; i < num1.length; i++) {
-  x.push(i)
-}
-
-for (let i = 0; i < num2.length; i++) {
-  y.push(i)
-}
-
-function listToNum(list) {
-  console.log('list ', list)
-  if (list.length > 0) {
-    return Number(list.join(''))
+function arrToNum(arr) {
+  if (arr.length > 0) {
+    return Number(arr.map(i => String(i)).join(''))
   } else {
     return 0;
   }
 }
 
-function numToList(num) {
+function numToArr(num) {
   let res = [];
   let stringifiedNum = String(num)
   for (let i = 0; i < stringifiedNum; i++) {
     res.push(stringifiedNum[i]);
   }
-  return res;
+  return res.map(i => Number(i));
 }
 
 function splitList(list) {
@@ -35,38 +23,52 @@ function splitList(list) {
   return { a: list.slice(0, half), b: list.slice(half) };
 }
 
+
+function numOfDigits(num) {
+  return String(num).length;
+}
+
+function getSplits(num) {
+  let arr = numToArr(num);
+  let half = Math.floor(arr.length/2);
+  let firstHalf = arr.slice(0, half)
+  let secondHalf = arr.slice(half)
+
+  return { firstHalf, secondHalf };
+}
+
 function karatsubaMult(x, y) {
-  console.log(x);
-  console.log(y);
-  if (x.length === 0) {
-    return 0;
-  } else if (x.length === 1) {
-    return Number(x[0]) * Number(y[0]);
+  if (numOfDigits(x) === 0) {
+    return 1;
+  } else if (numOfDigits(x) === 1) {
+    return x * y;
   } else {
-    let obj1 = splitList(x);
-    let obj2 = splitList(y);
+    let splitX = getSplits(x);
+    let splitY = getSplits(y);
 
-    let a = obj1.a;
-    let b = obj1.b;
-    let c = obj2.a;
-    let d = obj2.b;
+    let a = splitX.firstHalf;
+    let b = splitX.secondHalf;
+    let c = splitY.firstHalf;
+    let d = splitY.secondHalf;
 
-    let p = listToNum(a) + listToNum(b)
-    let p_list = numToList(p)
-    let q = listToNum(c) + listToNum(d)
-    let q_list = numToList(q)
+    let p = a + b
+    let q = c + d
 
-    let ac = karatsubaMult(a, c)
-    let bd = karatsubaMult(b, d)
-    let pq = karatsubaMult(p_list, q_list)
+    let ac = karatsubaMult(a, c);
+    console.log('ac ', ac)
+    let bd = karatsubaMult(b, d);
+    console.log('bd ', bd)
+    let pq = karatsubaMult(p, q);
+    console.log('pq ', pq)
 
-    let adbc = pq - ac - bd
+    let adbc = pq - ac - bd;
 
-    return (10^(len(x))) * ac + (10^(len(x)/2)) * adbc + bd
+    return (10^(numOfDigits(x))) * ac + (10^(numOfDigits(x)/2)) * adbc + bd;
   }
 }
 
 
 
-karatsubaMult([5,6,7,8], [1,2,3,4])
+let res = karatsubaMult(10, 12)
+console.log('res ', res);
 // recursiveMultiplication(x, y)
