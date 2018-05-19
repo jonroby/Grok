4d76af8fce68d9188b73f5db9324b2c0e63cf4a8
jonroby <joncaroby@gmail.com>
2018-05-19 02:12:44 -0700

    multi imports

--- ast/test/generateActions.js
+++ ast/test/generateActions.js
@@ -4,10 +4,17 @@ import { transform } from 'babel-core';
 const actionStr = fs.readFileSync('./actionStrings.js', 'utf8');
 
 function transformAction({ types: t, template }) {
-  const importX = template(`import x from "./x";`, { sourceType: "module" });
-  const exportX = template(`export const fetchUser = payload =>
-  ({ type: types.FETCH_USER, payload });
-`, { sourceType: "module" });
+//   const importX = template(`import x from "./x";`, { sourceType: "module" });
+//   const exportX = template(`export const fetchUser = payload =>
+//   ({ type: types.FETCH_USER, payload });
+// `, { sourceType: "module" });
+  let test = "yo";
+  const importX = template(`import { IMPORT_NAME, IMPORT_2 } from "${test}";`, { sourceType: "module" });
+
+  // const importX = buildRequire();
+
+  const importId = t.identifier("fetchSomething");
+  console.log('importId ', importId);
 
   return {
     visitor: {
@@ -15,11 +22,27 @@ function transformAction({ types: t, template }) {
         const lastImport = path.get("body").filter(p => p.isImportDeclaration()).pop();
         // console.log('lastImport ', lastImport)
 
-        const lastExport = path.get("body").filter(p => p.isExportDeclaration()).pop();
+        // const lastExport = path.get("body").filter(p => p.isExportDeclaration()).pop();
 
-        if (lastImport) lastImport.insertAfter(importX());
-        if (lastExport) lastExport.insertAfter(exportX());
+        // console.log('lastExport ', lastExport.get("body"))
+        console.log(1)
+
+
+        if (lastImport) {
+          lastImport.insertAfter(importX({
+            IMPORT_NAME: t.identifier('fetchSomething'),
+            IMPORT_2: t.identifier('fetchSomethingSuccess'),
+            SOURCE: t.stringLiteral("my-module")
+          }));
+        }
+        // if (lastExport) lastExport.insertAfter(exportX());
+        console.log(2)
       },
+      ExportNamedDeclaration(path, state) {
+        path.node.specifiers.push(importId);
+
+        // path.node.specifiers[1].insertAfter(exportVariable());
+      }
     }
   };
 };
@@ -29,3 +52,4 @@ const out = transform(actionStr, {
 });
 
 console.log(out.code);
+
