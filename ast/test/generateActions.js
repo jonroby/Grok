c3313589bc655f30e7a23ac0789ddf045d719440
jonroby <joncaroby@gmail.com>
2018-05-19 00:45:00 -0700

    ast for inserting imports and exports

--- ast/test/generateActions.js
+++ ast/test/generateActions.js
@@ -3,10 +3,23 @@ import { transform } from 'babel-core';
 
 const actionStr = fs.readFileSync('./actionStrings.js', 'utf8');
 
-function transformAction({ types: t }) {
+function transformAction({ types: t, template }) {
+  const importX = template(`import x from "./x";`, { sourceType: "module" });
+  const exportX = template(`export const fetchUser = payload =>
+  ({ type: types.FETCH_USER, payload });
+`, { sourceType: "module" });
+
   return {
     visitor: {
+      Program(path, state) {
+        const lastImport = path.get("body").filter(p => p.isImportDeclaration()).pop();
+        // console.log('lastImport ', lastImport)
 
+        const lastExport = path.get("body").filter(p => p.isExportDeclaration()).pop();
+
+        if (lastImport) lastImport.insertAfter(importX());
+        if (lastExport) lastExport.insertAfter(exportX());
+      },
     }
   };
 };
