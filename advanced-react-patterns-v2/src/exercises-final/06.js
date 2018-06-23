83c56a27e95ae4206da2131df1b7577daf483807
jonroby <joncaroby@gmail.com>
2018-06-22 17:14:36 -0700

    ex 6

--- advanced-react-patterns-v2/src/exercises-final/06.js
+++ advanced-react-patterns-v2/src/exercises-final/06.js
@@ -26,7 +26,7 @@ class Toggle extends React.Component {
     }
   }
   render() {
-    return this.props.children(this.getStateAndHelpers())
+    return this.props.children(this.getStateAndHelpers());
   }
 }
 
